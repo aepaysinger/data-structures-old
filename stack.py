40f8cb112d871090e8a35d579dcba8c00a3961dc
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-15 15:14:13 -0500

    changed importing LinkedList

--- stack.py
+++ stack.py
@@ -1,11 +1,9 @@
+from linked_list import LinkedList
+
+
 class Stack:
     def __init__(self, values=None):
-        self.head = None
-        self._length = 0
-
-        for value in values[::-1]:
-            self.head = Node(value, self.head)
-            self._length += 1
+        self.linked_list = LinkedList(values)
 
 
     def push(self, value):
@@ -13,8 +11,7 @@ class Stack:
         Adds a value to the stack. The parameter is the value to be added 
         to the stack.
         """
-        self.head = Node(value, self.head)
-        self._length += 1
+        self.linked_list.push(value)
     
     def pop(self):
         """
@@ -22,23 +19,9 @@ class Stack:
         If the stack is empty, attempts to call pop should raise an 
         appropriate Python exception with message.
         """
-        
-        try:
-            value = self.head.value
-        except AttributeError:
-            raise ValueError("There are no values to remove")
-        self.head = self.head.next
-        self._length -= 1
-        return value
 
+        return self.linked_list.pop()
 
-        
+    def __len__(self):
+        return self.linked_list.size()
 
-
-class Node:
-    def __init__(self, value, next):
-        self.value = value
-        self.next = next
-    
-    def __eq__(self, other):
-       return self.value == other.value
--- stack_test.py
+++ stack_test.py
@@ -1,23 +1,22 @@
 import pytest
 
-
-from stack import Node, Stack
+from stack import Stack
 
 
 def test_push():
     sl = Stack([1, 2, 3])
     sl.push(5)
 
-    assert sl.head.value == 5
-    assert sl._length == 4
+    assert sl.linked_list.head.value == 5
+    assert len(sl) == 4
 
 
 def test_pop():
     sl = Stack([1, 2, 3])
     sl.pop()
 
-    assert sl.head.value == 2
-    assert sl._length == 2
+    assert sl.linked_list.head.value == 2
+    assert len(sl) == 2
 
     sl.pop()
     sl.pop()
@@ -25,6 +24,5 @@ def test_pop():
     with pytest.raises(ValueError) as exc_info:
         sl.pop()
         assert exc_info.value.args[0] == "There are no values to remove"
-    assert sl._length == 0    
-
+    assert len(sl) == 0    
     
