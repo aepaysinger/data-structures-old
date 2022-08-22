9c8b8fdd822ffc1ae38d7cbbec9f60a75a02dbcf
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-22 15:07:24 -0500

    pausing on dll/tests

--- double_linked_list.py
+++ double_linked_list.py
@@ -1,30 +1,68 @@
 class DoubleLinkedList:
     def __init__(self, values):
         self.head = None  
-        self.previous = None
         self._length = 0
+        
 
         for value in values[::-1]:
-            self.head = Node(value, self.head, self.previous)
+            self.head = Node(value, self.head, None)
+            if self.head.next is not None:
+                self.head.next.previous = self.head
             self._length += 1
-
-
+            
 
     def push(self, value): 
         """
         will insert the value val at the head of the list
         """
-        self.head = Node(value, self.head, self.previous)
-        self._length += 1
+        self.head = Node(value, self.head, None)
+        if self.head.next is not None:
+            self.head.next.previous = self.head
+            self._length += 1
 
 
-    def append(self, value): #[1, 2, 3]4
+    def append(self, value): 
         """
         will append the value val at the tail of the list
         """
-        if self.next.value == None:
-            self.previous = self.next
-            self.next = self.next.next
+        current = self.head
+        while True:
+            if current.next is not None:
+                current = current.next
+            else:
+                break
+
+        current.next = Node(value, None, current)
+
+
+    # def pop(self): 
+    #     """
+    #     will pop the first value off the head of the list and return it.
+    #     Raises an exception with an appropriate message if there are no 
+    #     values to return.
+    #     """
+    #     try:
+    #         value = self.head.value
+    #     except AttributeError:
+    #         raise ValueError("There are no values to return")
+    #     self.head = self.head.next
+    #     self._length -= 1
+    #     return value
+
+
+    # def shift(self):[1,2,3]
+    #     """ 
+    #     will remove the last value from the tail of the list and return it. 
+    #     Raises an exception with an appropriate message if there are no values to 
+    #     return.
+    #     """
+    #     try:
+    #         value = self.head.value
+    #     except AttributeError:
+    #         raise ValueError("There are no values to return")
+
+        
+
 
 
 class Node:
--- double_linked_list_test.py
+++ double_linked_list_test.py
@@ -3,17 +3,46 @@ import pytest
 
 from double_linked_list import DoubleLinkedList, Node
 
+def test_dll_init():
+    dll = DoubleLinkedList([1, 2, 3])
+
+    assert dll.head.previous is None
+    assert dll.head.next.previous is dll.head
+    assert dll.head.value == 1
+    assert dll.head.next.next.value == 3
+    assert dll.head.next.next.next is None
+    assert dll.head.next.next.previous is dll.head.next
+
+
 
 def test_dll_push():
     dll = DoubleLinkedList([1 ,2 ,3])
     dll.push(4)
 
     assert dll.head.value == 4, "4 should be at the head of the list"
-    assert dll._length == 4, "there are 4 values."
+    assert dll._length == 4, f"***is should be ***"
+    assert dll.head.previous is None
+    assert dll.head.next.previous is dll.head
 
 
 def test_dll_append():
     dll = DoubleLinkedList([1, 2, 3])
     dll.append(4)
 
+    assert dll.head.value == 1
     assert dll.head.next.next.next.value == 4, "4 should now be the last value in the DoubleLinkedList."
+    assert dll.head.next.next.next.previous.value == 3
+
+# def test_dll_pop_1():
+#     dll = DoubleLinkedList([1, 2, 3])
+#     dll.pop()
+
+#     assert dll.head.value == 2
+
+ 
+# def test_dll_shift():
+#     dll = DoubleLinkedList([1, 2, 3])
+#     dll.shift()
+
+#     assert dll.head.next.next is None
+#     assert dll._length == 2
