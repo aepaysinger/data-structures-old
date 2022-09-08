ba78b95a8612fca879aeaed3d203317709bc39d5
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-08 15:29:12 -0500

    working on dll

--- double_linked_list.py
+++ double_linked_list.py
@@ -1,38 +1,27 @@
 class DoubleLinkedList:
     def __init__(self, values):
-        self.head = None  
+        self.head = None
         self._length = 0
+
+        for value in values:
+            self.head = Node(value, next, previous)
+            
         
 
-        for value in values[::-1]:
-            self.head = Node(value, self.head, None)
-            if self.head.next is not None:
-                self.head.next.previous = self.head
-            self._length += 1
             
 
     def push(self, value): 
         """
         will insert the value val at the head of the list
         """
-        self.head = Node(value, self.head, None)
-        if self.head.next is not None:
-            self.head.next.previous = self.head
-            self._length += 1
+      
 
 
     def append(self, value): 
         """
         will append the value val at the tail of the list
         """
-        current = self.head
-        while True:
-            if current.next is not None:
-                current = current.next
-            else:
-                break
-
-        current.next = Node(value, None, current)
+     
 
 
     # def pop(self): 
@@ -41,13 +30,7 @@ class DoubleLinkedList:
     #     Raises an exception with an appropriate message if there are no 
     #     values to return.
     #     """
-    #     try:
-    #         value = self.head.value
-    #     except AttributeError:
-    #         raise ValueError("There are no values to return")
-    #     self.head = self.head.next
-    #     self._length -= 1
-    #     return value
+    #     
 
 
     # def shift(self):[1,2,3]
@@ -56,10 +39,7 @@ class DoubleLinkedList:
     #     Raises an exception with an appropriate message if there are no values to 
     #     return.
     #     """
-    #     try:
-    #         value = self.head.value
-    #     except AttributeError:
-    #         raise ValueError("There are no values to return")
+  
 
         
 
@@ -71,5 +51,7 @@ class Node:
         self.next = next
         self.previous = previous
 
-    def __eq__(self, other):
-        return self.value == other.value
+
+
+
+  
