e0911f9cd49ec974b10036a5916a0c311c8d8859
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-20 13:58:49 -0500

    double linked list completed, updated README

--- double_linked_list.py
+++ double_linked_list.py
@@ -1,48 +1,77 @@
 class DoubleLinkedList:
-    def __init__(self, values):
+    def __init__(self, values=None):
         self.head = None
         self._length = 0
+        if values:
+            for value in values:
+                self.head = Node(value, self.head, None)
+                if self.head.next:
+                    self.head.next.previous = self.head
+                self._length += 1
 
-        for value in values:
-            self.head = Node(value, next, previous)
-            
-        
-
-            
 
     def push(self, value): 
         """
         will insert the value val at the head of the list
         """
-      
+        
+        self.head = Node(value, self.head, None)
+        if self.head.next:
+            self.head.next.previous = self.head
+        self._length += 1
 
 
     def append(self, value): 
         """
         will append the value val at the tail of the list
         """
-     
-
-
-    # def pop(self): 
-    #     """
-    #     will pop the first value off the head of the list and return it.
-    #     Raises an exception with an appropriate message if there are no 
-    #     values to return.
-    #     """
-    #     
-
-
-    # def shift(self):[1,2,3]
-    #     """ 
-    #     will remove the last value from the tail of the list and return it. 
-    #     Raises an exception with an appropriate message if there are no values to 
-    #     return.
-    #     """
-  
-
         
+        node = self.head
+        if not node:
+            self.head = Node(value, self.head, None)
+            self._length += 1
+            return
+        while node.next:
+            node = node.next
+           
+        node.next = Node(value, None, node)
+        self._length += 1
+        
+        
+    def pop(self): 
+        """
+        will pop the first value off the head of the list and return it.
+        Raises an exception with an appropriate message if there are no 
+        values to return.
+        """
 
+        if self.head:
+            old_head = self.head
+            self.head = self.head.next
+            self.head.previous = None
+            self._length -= 1
+            
+            return old_head.value
+        else:
+            raise ValueError("empty list")
+       
+
+    def shift(self):
+        """ 
+        will remove the last value from the tail of the list and return it. 
+        Raises an exception with an appropriate message if there are no values to 
+        return. 
+        """
+
+        node = self.head
+        if not node:
+            raise ValueError("empty list")
+        while node.next:
+            node = node.next
+        node.previous.next = None  
+        self._length -= 1 
+        return node.value
+            
 
 
 class Node:
@@ -50,7 +79,8 @@ class Node:
         self.value = value
         self.next = next
         self.previous = previous
-
+        #previous is in the Node
+        
 
 
 
--- double_linked_list_test.py
+++ double_linked_list_test.py
@@ -6,43 +6,74 @@ from double_linked_list import DoubleLinkedList, Node
 def test_dll_init():
     dll = DoubleLinkedList([1, 2, 3])
 
-    assert dll.head.previous is None
-    assert dll.head.next.previous is dll.head
-    assert dll.head.value == 1
-    assert dll.head.next.next.value == 3
+    # assert dll.head.previous is None
+    # assert dll.head.next.previous is dll.head
+    assert dll.head.value == 3
+    assert dll.head.next.next.value == 1
     assert dll.head.next.next.next is None
     assert dll.head.next.next.previous is dll.head.next
 
 
-
 def test_dll_push():
     dll = DoubleLinkedList([1 ,2 ,3])
-    dll.push(4)
+    dll.push(4) #[1, 2, 3, 4]
 
-    assert dll.head.value == 4, "4 should be at the head of the list"
-    assert dll._length == 4, f"***is should be ***"
+    assert dll.head.value == 4
+    assert dll._length == 4
     assert dll.head.previous is None
     assert dll.head.next.previous is dll.head
+    assert dll.head.next.value == 3
+
+
+def test_dll_append_empty():
+    dll = DoubleLinkedList([])
+    dll.append(4)
+
+    assert dll.head.value == 4
+    assert dll._length == 1
 
 
 def test_dll_append():
     dll = DoubleLinkedList([1, 2, 3])
-    dll.append(4)
+    dll.append(4) 
 
-    assert dll.head.value == 1
-    assert dll.head.next.next.next.value == 4, "4 should now be the last value in the DoubleLinkedList."
-    assert dll.head.next.next.next.previous.value == 3
+    assert dll.head.value == 3
+    assert dll.head.next.next.next.value == 4
+    assert dll.head.next.next.next.previous.value == 1
 
-# def test_dll_pop_1():
-#     dll = DoubleLinkedList([1, 2, 3])
-#     dll.pop()
+def test_dll_pop_1():
+    dll = DoubleLinkedList([1, 2, 3])
+    assert dll.pop() == 3
+    assert dll.head.value == 2
+    assert dll._length == 2
+    assert dll.head.previous is None
+    assert dll.head.next.value == 1
+    
 
-#     assert dll.head.value == 2
+def test_dll_pop_empty():
+    dll = DoubleLinkedList()
+    
+    with pytest.raises(ValueError) as excinfo:
+        dll.pop()
 
- 
-# def test_dll_shift():
-#     dll = DoubleLinkedList([1, 2, 3])
-#     dll.shift()
+    assert str(excinfo.value) == "empty list"
+    
 
-#     assert dll.head.next.next is None
-#     assert dll._length == 2
+def test_dll_shift():
+    dll = DoubleLinkedList([1, 2, 3])
+    dll.shift()
+
+    assert dll.head.next.next is None
+    assert dll._length == 2
+    assert dll.head.value == 3
+    assert dll.head.next.value == 2
+    assert dll.head.previous is None
+
+
+def test_dll_shift_empty():
+    dll = DoubleLinkedList()
+
+    with pytest.raises(ValueError) as excinfo:
+        dll.shift()
+
+    assert str(excinfo.value) == "empty list"
