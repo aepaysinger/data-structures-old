ed9d2850f7fc88c097ce1b14b281478ce3351a49
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-13 13:02:04 -0600

    queue

--- que_.py
+++ que_.py
@@ -2,25 +2,26 @@ from linked_list import LinkedList
 
 
 class Que_():
-    def __init__(self, values=None):
-        self.linked_list = LinkedList(values)
+    def __init__(self, items):
+        self.items = items
+
 
 
     def enqueue(self, value):
-        self.linked_list.push(value)
+        self.items.append(value)
 
     
-    def dequeue(self):
-        self.linked_list.pop()
+    # def dequeue(self):
+    #     self.linked_list.pop()
 
     
-    def peek(self):
-        if len(self.linked_list) >= 2:
-            return self.linked_list.head.next.value
-        else:
-            return None
+    # def peek(self):
+    #     if len(self.linked_list) >= 2:
+    #         return self.linked_list.head.next.value
+    #     else:
+    #         return None
 
     
-    def size(self):
-        return len(self.linked_list)
+    # def size(self):
+    #     return len(self.linked_list)
 
