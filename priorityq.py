01e8a87621961285007de3c9fefe3b9abf634df2
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-27 12:41:51 -0600

    priorityq

---
+++ priorityq.py
@@ -0,0 +1,17 @@
+fro
+
+class PriorityQueue:
+    def __init__(self, values=None):
+        pass
+
+    def insert(self, value):
+        # inserts a value into the queue. Takes an optional argument for that value’s priority, set by default to whatever your lowest priority is (i.e. 0, -99, whatever)
+        pass
+
+    def pop(self):
+        # removes the most important item from the queue and returns its value.
+        pass
+
+    def peek(self):
+        # returns the most important item without removing it from the queue.
+        pass
