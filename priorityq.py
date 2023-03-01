c4fb29f6679e094e28eb21984e31b716ffcc24ae
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-01 14:03:49 -0600

    using bionary search

--- priorityq.py
+++ priorityq.py
@@ -1,31 +1,68 @@
+
+
 class PriorityQueue:
     def __init__(self):
         self.storage = []
+        
 
-    def insert(self, value, priority=0):
-        if self.storage == []:
-            self.storage.append((priority, value))
-        else:
-            if priority:
-                for i in range(len(self.storage) + 1):
-                    print(i, self.storage[i])
-                    if priority < self.storage[i][0]:
-                        self.storage.insert(i, (priority, value))
-                        break
-                    elif i == len(self.storage) - 1:
-                        self.storage.append((priority, value))
-                        break
-                    elif (
-                        priority > self.storage[i][0]
-                        and priority < self.storage[i + 1][0]
-                    ):
-                        self.storage.insert(i + 1, (priority, value))
-                        break
-            else:
-                self.storage.append((self.storage[-1][0], value))
+    # def insert(self, value, priority=0):
+    #     if self.storage == []:
+    #         self.storage.append((priority, value))
+    #     else:
+    #         if priority:
+    #             for i in range(len(self.storage) + 1):
+    #                 print(i, self.storage[i])
+    #                 if priority < self.storage[i][0]:
+    #                     self.storage.insert(i, (priority, value))
+    #                     break
+    #                 elif i == len(self.storage) - 1:
+    #                     self.storage.append((priority, value))
+    #                     break
+    #                 elif (
+    #                     priority > self.storage[i][0]
+    #                     and priority < self.storage[i + 1][0]
+    #                 ):
+    #                     self.storage.insert(i + 1, (priority, value))
+    #                     break
+    #         else:
+    #             self.storage.append((self.storage[-1][0], value))
 
     def pop(self):
         return self.storage.pop(0)
 
     def peek(self):
         return self.storage[0]
+    
+    def _insert(self, value, priority=0, left=0, right=None):
+        if self.storage == []:
+            self.storage.append((priority, value))
+        else:
+            if priority:
+                left = 0
+                right = len(self.storage) - 1
+                mid = (left + right) // 2
+
+                if priority > self.storage[-1][0]:
+                    self.storage.append((priority, value))
+                elif priority < self.storage[0][0]:
+                    self.storage.insert(0, (priority, value))
+                elif self.storage[mid][0] == priority:
+                    for i in range(mid, len(self.storage)):
+                        if self.storage[i+1] > priority:
+                            self.storage.insert(i +1, (priority, value))
+                elif self.storage[mid][0] < priority and self.storage[mid + 1][0] > priority:
+                    self.storage.insert(mid + 1, (priority, value))
+                elif priority > mid:
+                    return self._insert(self, priority, left, mid)
+                elif priority < mid:
+                    return self._insert(self, priority, mid, right)
+            else:
+                self.storage.append((self.storage[-1][0], value))
+       
+
+    
+    
+
+
+        
+# [-3, 0, 1, 1, 5, 7, 10] insert (1)
--- priorityq_test.py
+++ priorityq_test.py
@@ -3,24 +3,24 @@ from priorityq import PriorityQueue
 
 def test_priority_queue_insert():
     priorityq = PriorityQueue()
-    priorityq.insert("aa", -1)
-    priorityq.insert("b", 2)
-    priorityq.insert("a", 1)
-    priorityq.insert("c", 3)
+    priorityq._insert("aa", -1)
+    priorityq._insert("b", 2)
+    priorityq._insert("a", 1)
+    priorityq._insert("c", 3)
 
     assert priorityq.storage == [(-1, "aa"), (1, "a"), (2, "b"), (3, "c")]
 
-    priorityq.insert("f")
+    priorityq._insert("f")
 
     assert priorityq.storage == [(-1, "aa"), (1, "a"), (2, "b"), (3, "c"), (3, "f")]
 
 
 def test_priority_queue_pop():
     priorityq = PriorityQueue()
-    priorityq.insert(55, 1)
-    priorityq.insert("alpha", -3)
-    priorityq.insert("bravo")
-    priorityq.insert(4, 2)
+    priorityq._insert(55, 1)
+    priorityq._insert("alpha", -3)
+    priorityq._insert("bravo")
+    priorityq._insert(4, 2)
 
     assert priorityq.pop() == (-3, "alpha")
     assert priorityq.storage == [(1, 55), (1, "bravo"), (2, 4)]
@@ -28,8 +28,8 @@ def test_priority_queue_pop():
 
 def test_priority_queue_peek():
     priorityq = PriorityQueue()
-    priorityq.insert("alpha")
-    priorityq.insert("beta")
+    priorityq._insert("alpha")
+    priorityq._insert("beta")
 
     assert priorityq.peek() == (0, "alpha")
 
