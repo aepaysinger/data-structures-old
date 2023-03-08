0db091f31d91f9b5e7782fd840c036b67e1efa82
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-08 12:12:41 -0600

    cleaned up, adjust smallest priority

--- priorityq.py
+++ priorityq.py
@@ -1,26 +1,18 @@
+import bisect
+import math
+
+
 class PriorityQueue:
     def __init__(self):
         self.storage = []
 
-    def insert(self, value, priority=0):
+    def insert(self, value, priority=-math.inf):
         if self.storage == []:
             self.storage.append((priority, value))
-        elif priority:
-            for i in range(len(self.storage) + 1):
-                if priority > self.storage[i][0]:
-                    self.storage.insert(i, (priority, value))
-                    break
-                elif i == len(self.storage) - 1:
-                    self.storage.append((priority, value))
-                    break
-                elif (
-                    priority < self.storage[i][0]
-                    and priority > self.storage[i + 1][0]
-                ):
-                    self.storage.insert(i + 1, (priority, value))
-                    break
-        else:
-            self.storage.insert(0, (self.storage[0][0], value))
+            return
+        for i in range(len(self.storage) + 1):
+            bisect.insort_left(self.storage, (priority, value), key=lambda t: t[0])
+            break
 
     def pop(self):
         return self.storage.pop()
--- priorityq_test.py
+++ priorityq_test.py
@@ -1,4 +1,5 @@
 from priorityq import PriorityQueue
+import math
 
 
 def test_priority_queue_insert():
@@ -8,11 +9,17 @@ def test_priority_queue_insert():
     priorityq.insert("a", 1)
     priorityq.insert("c", 3)
 
-    assert priorityq.storage == [(3, "c"), (2, "b"), (1, "a"), (-1, "aa")]
+    assert priorityq.storage == [(-1, "aa"), (1, "a"), (2, "b"), (3, "c")]
 
     priorityq.insert("f")
 
-    assert priorityq.storage == [(3, "f"), (3, "c"), (2, "b"), (1, "a"), (-1, "aa")]
+    assert priorityq.storage == [
+        (-math.inf, "f"),
+        (-1, "aa"),
+        (1, "a"),
+        (2, "b"),
+        (3, "c"),
+    ]
 
 
 def test_priority_queue_pop():
@@ -22,8 +29,8 @@ def test_priority_queue_pop():
     priorityq.insert("bravo")
     priorityq.insert(4, 2)
 
-    assert priorityq.pop() == (-3, "alpha")
-    assert priorityq.storage == [(2, 4), (1, "bravo"), (1, 55)]
+    assert priorityq.pop() == (2, 4)
+    assert priorityq.storage == [(-math.inf, "bravo"), (-3, "alpha"), (1, 55)]
 
 
 def test_priority_queue_peek():
@@ -31,6 +38,6 @@ def test_priority_queue_peek():
     priorityq.insert("alpha")
     priorityq.insert("beta")
 
-    assert priorityq.peek() == (0, "alpha")
+    assert priorityq.peek() == (-math.inf, "alpha")
 
-    assert priorityq.storage == [(0, "beta"), (0, "alpha")]
+    assert priorityq.storage == [(-math.inf, "beta"), (-math.inf, "alpha")]
